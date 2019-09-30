8272d733d43ac05c80abf0e85a41e7723f8ae55d
LongNC <long.nguyen2@gigasource.io>
2019-09-30 18:57:42 +0700

    modify send-message, redundant slash

--- patch_process/send-message.js
+++ patch_process/send-message.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require(".//service-account-file.json");
+const serviceAccount = require("./service-account-file.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),

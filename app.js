e41c4740c1cc88f1e7f6ba236e029644eb0a0288
LongNC <long.nguyen2@gigasource.io>
2019-10-01 13:55:36 +0700

    change route path to /static-apk/<topic>/<version>/<apk-name>

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/static-apk/:version/:name', (req, res) => {
-	const exist = fs.existsSync(`${__dirname}/public/${req.params.version}/${req.params.name}`);
-	console.log(`${__dirname}/public/${req.params.version}/${req.params.name} ${exist ? 'existed' : 'not existed'}`);
+app.get('/static-apk/:topic/:version/:name', (req, res) => {
+	const filePath = `${req.params.topic}/${req.params.version}/${req.params.name}`;
+	const exist = fs.existsSync(`${__dirname}/public/${filePath}`);
+	console.log(`${__dirname}/public/${filePath} ${exist ? 'existed' : 'not existed'}`);
 
-	if (fs.existsSync(`${__dirname}/public/${req.params.version}/${req.params.name}`)) {
+	if (fs.existsSync(`${__dirname}/public/${filePath}`)) {
 		const options = {
-			root: __dirname + '/public/' + req.params.version,
+			root: `${__dirname}/public/${req.params.topic}/${req.params.version}`,
 			dotFiles: 'deny',
 			headers: {
 				'x-timestamp': Date.now(),
@@ -19,7 +20,7 @@ app.get('/static-apk/:version/:name', (req, res) => {
 		const fileName = req.params.name;
 		res.sendFile(fileName, options, (err) => {
 			if (err) console.log(err);
-			else console.log(`Sent: ${req.params.version}/${fileName}`);
+			else console.log(`Sent: ${filePath}`);
 		});
 	} else {
 		res.send('File not found!');

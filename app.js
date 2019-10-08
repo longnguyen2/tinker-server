d9cfc8e979210351c1853729e06b54eb5f8ebf9c
LongNC <long.nguyen2@gigasource.io>
2019-10-08 18:54:07 +0700

    update md5 route

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
+const md5 = require('md5');
 const express = require('express');
 const app = express();
 
-app.get('/static-apk/:topic/:version/:name', (req, res) => {
-	const filePath = `${req.params.topic}/${req.params.version}/${req.params.name}`;
-	const exist = fs.existsSync(`${__dirname}/public/${filePath}`);
-	console.log(`${__dirname}/public/${filePath} ${exist ? 'existed' : 'not existed'}`);
+app.use(express.json());
 
-	if (fs.existsSync(`${__dirname}/public/${filePath}`)) {
+app.get('/static-apk/:topic/:version/:name', (req, res) => {
+	const apkPath = `${__dirname}/public/${req.params.topic}/${req.params.version}/${req.params.name}`;
+	const exist = fs.existsSync(apkPath);
+	console.log(`${apkPath} ${exist ? 'existed' : 'not existed'}`);
+
+	if (exist) {
 		const options = {
 			root: `${__dirname}/public/${req.params.topic}/${req.params.version}`,
 			dotFiles: 'deny',
@@ -20,11 +23,28 @@ app.get('/static-apk/:topic/:version/:name', (req, res) => {
 		const fileName = req.params.name;
 		res.sendFile(fileName, options, (err) => {
 			if (err) console.log(err);
-			else console.log(`Sent: ${filePath}`);
+			else console.log(`Sent: ${apkPath}`);
 		});
 	} else {
 		res.send('File not found!');
 	}
+});
+
+app.get('/md5/:topic/:version/:name', (req, res) => {
+	const apkPath = `${__dirname}/public/${req.params.topic}/${req.params.version}/${req.params.name}`;
+	const exist = fs.existsSync(apkPath);
+	if (exist) {
+		const buf = fs.readFileSync(apkPath);
+		res.status(200).send(md5(buf));
+	} else {
+		res.status(404).send('File not found!');
+	}
+});
+
+app.post('/report', (req, res) => {
+	const token = req.body.token;
+	console.log(`${token} returned a failed result`);
+	res.send('Retry patching in 5s');
 });
 
 app.get('/', (req, res) => {
